407debd5be6ae8fb30510cbdb429bfc0c8a72a6f
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-01-19 02:25:30 -0500

    #2

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     let quiz = [
-        "Four + Two is equal to Six.",
-        "Five - Three is  greater than One.",
-        "Three + Eight is less than Ten."
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is  greater than One.", "True"],
+        ["Three + Eight is less than Ten.", "False"]
     ]
     
     var questionNumber = 0
@@ -32,12 +32,26 @@ class ViewController: UIViewController {
     
     // MARK: Actioms
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+            updateUI()
+        } else {
+            questionNumber = 0
+        }
         updateUI()
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 
